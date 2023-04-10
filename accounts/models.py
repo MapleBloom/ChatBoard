b11a64253789669bcdb4c12e9af47534fbdc5041
maxmar <maxmar@yandex.ru>
2023-04-10 05:33:26 +0300

    Patch pages with empty avatars

--- accounts/models.py
+++ accounts/models.py
@@ -19,11 +19,12 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.avatar.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.avatar.path)
+        if self.avatar:
+            img = Image.open(self.avatar.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300,300)
+                img.thumbnail(output_size)
+                img.save(self.avatar.path)
 
 
 @receiver(post_save, sender=User)
